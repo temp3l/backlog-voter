81211bb200a482910abc6102fbb7e0bcad2e4045
steph <stephanmettler@gmx.de>
2019-05-22 20:48:01 +0200

    added score, humanScore and prozent

--- backlog-voter-js/src/components/Reports/Reporter.js
+++ backlog-voter-js/src/components/Reports/Reporter.js
@@ -20,17 +20,24 @@ function Reporter(props) {
 
       setReportItems(
         reportItems.data.map(item =>
-          Object.assign(item, { value: getRandomInt(30) })
+          Object.assign(item, { value: getRandomInt(2) })
         )
       );
       let interval = setInterval(() => {
         setReportItems(
           reportItems.data.map(item =>
-            Object.assign(item, { value: item.value + 0.28 })
+            Object.assign(item, { value: Math.round(item.value + 1) })
           )
         );
-      }, 20);
-      setTimeout(() => clearInterval(interval), 3500);
+      }, 100);
+      setTimeout(() => {
+        clearInterval(interval);
+        setReportItems(
+          reportItems.data.map(item =>
+            Object.assign(item, { value: 10 })
+          )
+        );
+      }, 1000);
     };
     fetchData();
   }, [backlogId]);
@@ -41,12 +48,34 @@ function Reporter(props) {
     setReportItems(obj);
   };
 
+
+
+
+const calculateResults = (reportItems) => {
+  console.log(reportItems)
+  let totalScore = 0;
+  let score = 0;
+  reportItems.forEach(item => {
+    totalScore += 20 * item.modifier;
+    score += Number( item.value) * Number(item.modifier );
+  });
+  let scoreP = score * 100 / totalScore;
+  return { 
+    totalScore: Math.round(totalScore/reportItems.length),
+    score: Math.round(score/reportItems.length), 
+    scoreP: Math.round(scoreP),
+    humanScore: Math.round(20/100*scoreP )
+  }
+}
+
+
   const submitReport = async () => {
     let report = Object.assign(
       { backlogId: backlog.id, userId: props.session.id },
-      { data: reportItems }
+      { data: reportItems, results: calculateResults(reportItems) },
     );
-    console.log(report);
+      alert(JSON.stringify(report.results,undefined,4))
+    
     try {
       const { data } = await api.post("/users/me/reports", report);
       props.history.push("/backlogs");
@@ -59,27 +88,27 @@ function Reporter(props) {
   return (
     <div className="container-fluid Slider">
       <h3>Create a report for: {backlog.name} </h3>
-      <p className="description">{backlog.desc}</p>
+      <p className="description">{backlog.description}</p>
 
       <div className="container-fluid">
         {reportItems.map((item, idx) => (
           <div className="row SliderRow" key={item.id}>
-            <div className="col col-md-2">
+            <div className="col col-md-2 col-sm-12">
               <h4>{item.name}</h4>
             </div>
-            <div className="col col-md-9 slider">
-              <div className="text-center description">{item.desc}</div>
+            <div className="col col-md-9 col-sm-10 slider">
+              <div className="text-center description">{item.description}</div>
               <input
                 type="range"
                 className="custom-range"
                 min="0"
-                max="100"
+                max="20"
                 value={item.value}
                 onChange={e => onChange(idx, e.target.value)}
               />
             </div>
-            <div className="col col-md-1">
-              <h4>{item.value}</h4>
+            <div className="col col-md-1 col-sm-2">
+              <h4> <span className="badge badge-secondary">{item.value}</span></h4>
             </div>
           </div>
         ))}
@@ -96,16 +125,5 @@ function Reporter(props) {
     </div>
   );
 }
-/*
-  <table class="table table-hover">
-    <tbody>
-    {reportItems.map( (item, idx) => (<tr>
-        <td><h4>{item.name}</h4></td>
-        <td class="slider"><input type="range" className="custom-range mySlide" min="0" max="100" value={item.value} onChange={e => onChange(idx, e.target.value)}/></td>
-        <td> <h4>{item.value}</h4></td>
-      </tr>))}
-    </tbody>
-  </table>
-*/
-//onChange={e => onChange(item.name, e)}
+
 export default Reporter;
